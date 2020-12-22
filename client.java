5efaad7abdc0bf0d3218cbe1171ac3eb0a95505b
at.moharm <62400652+atmoharam@users.noreply.github.com>
2020-12-23 01:05:55 +0200

    Update client.java

--- client.java
+++ client.java
@@ -6,58 +6,41 @@ import java.net.Socket;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
+ 
 public class Client {
-
+ 
     Client(){
         try {
             InetAddress ip = InetAddress.getLocalHost();
             System.out.println(ip);
             Socket other = new Socket(ip , 22000);
             Scanner scan =new Scanner(System.in);
-
+ 
             DataInputStream otherReadSource = new  DataInputStream(other.getInputStream());
             DataOutputStream othertWriteSource = new DataOutputStream(other.getOutputStream());
-
+ 
             String str = " ";
-            while(true)
-            {
+            while(true){
+ 
                 str = otherReadSource.readUTF();
                 System.out.println(str);
                 str = otherReadSource.readUTF();
                 System.out.println(str);
-                if (str.equalsIgnoreCase("Exite")) {
-                    break;
-                }
-                for (int i = 0; i < 10; i++) {
-                    int item = scan.nextInt();
-                    othertWriteSource.writeInt(item);
-                }
+ 
+                str=scan.nextLine();
+                othertWriteSource.writeUTF(str);
+                if(str.equalsIgnoreCase("Exite")){break;}
                 str = otherReadSource.readUTF();
-                System.out.println(str);
-                for (int i = 0; i < 10; i++) {
-                    int item = otherReadSource.readInt();
-                    System.out.println(item);
-                }
-                str = otherReadSource.readUTF();
-                System.out.println(str);
-                for (int i = 0; i < 10; i++) {
-                    int item = otherReadSource.readInt();
-                    System.out.println(item);
-                }
-                str = otherReadSource.readUTF();
-                System.out.println(str);
-
-            }
-
+                System.out.println(str);}
+ 
             otherReadSource.close();
             othertWriteSource.close();
             other.close();
-
+ 
         } catch (IOException ex) {
             Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
         }
-
+ 
     }
-
+ 
 }
